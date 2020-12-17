6896fcbabbe80d573a1dda09265d6333d0601893
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2020-12-17 16:14:03 -0500

    start models for django chess

--- django-chess-example/game/models.py
+++ django-chess-example/game/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+COLOR_CHOICES = [
+    ('BL', 'Black'), 
+    ('WH', 'White')
+]
+
+TYPE_NAME_CHOICES = [
+    ('K', 'King'), 
+    ('Q', 'Queen'),
+    ('B', 'Bishop'),
+    ('P', 'Pawn'), 
+    ('R', 'Rook'), 
+    ('KN', 'Knight')
+]
+
+class Square(models.Model):
+    letter = models.CharField(max_length=1)
+    number = models.IntegerField()
+
+
+class Type(models.Model):
+    name = models.CharField(choices=TYPE_NAME_CHOICES, max_length=10)
+    rule = models.CharField(max_length=255)
+
+
+class Piece(models.Model):
+    square = models.ForeignKey('Square', default=None, on_delete=models.CASCADE)
+    color = models.CharField(choices=COLOR_CHOICES, max_length=10)
+    type = models.ForeignKey('Type', on_delete=models.CASCADE)
+
